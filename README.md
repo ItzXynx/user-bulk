e9f98dc5380f82afb4e8c331aa338f37961c1c71
ItzXynx <xiaoleehong.dev@gmail.com>
2026-03-19 16:12:54 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# discord-user-info-bulk
+fetch info for multiple users at once
+
+## usage
+```
+python main.py TOKEN ID1 ID2 ID3
+```
---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import urllib.request
+import json
+
+def get_user(token, uid):
+    req = urllib.request.Request(
+        f"https://discord.com/api/v9/users/{uid}",
+        headers={"Authorization": token}
+    )
+    with urllib.request.urlopen(req) as r:
+        return json.loads(r.read())
+
+if __name__ == "__main__":
+    token = sys.argv[1]
+    ids = sys.argv[2:]
+    for uid in ids:
+        try:
+            u = get_user(token, uid)
+            print(f"{uid}: {u.get('username')} flags={u.get('public_flags', 0)}")
+        except:
+            print(f"{uid}: not found")
